beb2a30274b00b9438ad0e5cd62c4f46737e6001
huyenpoo <heartbolide@gmail.com>
2022-04-02 12:55:27 +0700

    add api-certificate and fix api-grocery

--- BackEnd/routes/groceryApis.py
+++ BackEnd/routes/groceryApis.py
@@ -1,44 +1,63 @@
 from routes import app, manager_required
 from bson import ObjectId
 from flask import request
+from .pagination import pagination
 from models.grocery import Grocery
-import pymongo
 
 grocery_collection = app.db.grocery
 
 
-@app.route('/grocery', methods=['GET'])
+@app.route('/grocery/<string:type_search', methods=['GET'])
 @manager_required("level_one")
-def get_grocery(current_manager=None):
+def get_grocery(current_manager=None, type_search: str = ''):
+    offset = int(request.args['offset'])
+    limit = int(request.args['limit'])
+    search_value = request.args['value']
+
+    list_groceries = []
     try:
-        list_of_grocery = grocery_collection.find({})
-        groceries = []
+        if search_value != '' and type_search != '':
+            all_groceries = grocery_collection.find({
+                type_search: {'$regex': f'^{search_value}', '$options': "m"}
+            })
+            for gr in all_groceries:
+                list_groceries.append(gr)
 
-        for gr in list_of_grocery:
-            groceries.append(gr)
-
-        return {'Result': 'Success',
-                'List_of_groceries': groceries}
-
-    except Exception as e:
-        return {'Result': 'Fail',
-                'Message': 'Can not get data'}
-
-
-@app.route('/grocery/<string:grocery_id>', methods=['GET'])
-@manager_required("level_one")
-def get_grocery_by_id(current_manager=None, grocery_id: str = ''):
-    try:
-        grocery = grocery_collection.find_one({'_id': ObjectId(grocery_id)})
-        if grocery:
-            return {'Result': 'Success',
-                    'Grocery': grocery}
+            return {'Status': 'Success',
+                    'List_of_groceries': pagination(path_dir=f'/grocery/{type_search}',
+                                                    offset=offset,
+                                                    limit=limit,
+                                                    list_database=list_groceries)}
         else:
-            return {'Result': 'Fail',
-                    'Message': 'Not found'}
+            all_groceries = grocery_collection.find({})
+            for gr in all_groceries:
+                list_groceries.append(gr)
+
+            return {'Status': 'Success',
+                    'List_of_groceries': pagination(path_dir=f'/grocery',
+                                                    offset=offset,
+                                                    limit=limit,
+                                                    list_database=list_groceries)}
+
     except Exception as e:
-        return {'Result': 'Fail',
-                'Message': 'Can not get data'}
+        return {'Status': 'Fail',
+                'Message': 'Can not get data'}, 400
+
+
+@app.route('/grocery/<string:name>', methods=['GET'])
+@manager_required("level_one")
+def get_grocery_by_name(current_manager=None, grocery_name: str = ''):
+    try:
+        grocery = grocery_collection.find_one({'name': grocery_name})
+        if grocery:
+            return {'Status': 'Success',
+                    'Grocery': grocery.to_dict()}
+        else:
+            return {'Status': 'Fail',
+                    'Message': 'Not found'}, 401
+    except Exception as e:
+        return {'Status': 'Fail',
+                'Message': 'Can not get data'}, 400
 
 
 @app.route('/grocery', methods=['POST'])
@@ -54,16 +73,16 @@ def add_grocery(current_manager=None):
                           certificate=data['certificate'])
     try:
         grocery_collection.insert_one(new_grocery.to_dict())
-        return {'Result': 'Success',
+        return {'Status': 'Success',
                 'Message': 'Add successfully'}
     except Exception as e:
-        return {'Result': 'Fail',
-                'Message': 'Can not insert data'}
+        return {'Status': 'Fail',
+                'Message': 'Can not insert data'}, 400
 
 
-@app.route('/grocery/<string:grocery_id>', methods=['PUT'])
+@app.route('/grocery/<string:grocery_name>', methods=['PUT'])
 @manager_required("level_one")
-def update_a_grocery(current_manager=None, grocery_id: str = ''):
+def update_a_grocery(current_manager=None, grocery_name: str = ''):
     data = request.get_json()
     updated_grocery = {'name': data['name'],
                        'owner': data['owner'],
@@ -73,27 +92,27 @@ def update_a_grocery(current_manager=None, grocery_id: str = ''):
                        'status': data['status'],
                        'certificate': data['certificate']}
     try:
-        grocery_collection.find_one_and_update({'_id': ObjectId(grocery_id)},
+        grocery_collection.find_one_and_update({'name': grocery_name},
                                                {"$set": updated_grocery})
-        return {'Result': 'Success',
+        return {'Status': 'Success',
                 'Message': 'Update successfully'}
     except Exception as e:
         print(e)
-        return {'Result': 'Fail',
-                'Message': 'Can not update'}
+        return {'Status': 'Fail',
+                'Message': 'Can not update'}, 400
 
 
-@app.route('/grocery/<string:grocery_id>', methods=['DELETE'])
+@app.route('/grocery/<string:grocery_name>', methods=['DELETE'])
 @manager_required("level_one")
-def delete_grocery(current_manager=None, grocery_id: str = ''):
+def delete_grocery(current_manager=None, grocery_name: str = ''):
     try:
-        deleted_grocery = grocery_collection.find_one_and_delete({'_id': ObjectId(grocery_id)})
+        deleted_grocery = grocery_collection.find_one_and_delete({'name': grocery_name})
         if deleted_grocery:
-            return {'Result': 'Success',
-                    'Message': f'Deleted {grocery_id}'}
+            return {'Status': 'Success',
+                    'Message': f'Deleted {grocery_name}'}
         else:
-            return {'Result': 'Fail',
-                    'Message': f'Do not have grocery {grocery_id} in database'}
+            return {'Status': 'Fail',
+                    'Message': f'Do not have grocery {grocery_name} in database'}, 401
     except Exception as e:
-        return {'Result': 'Fail',
-                'Message': 'Can not delete'}
+        return {'Status': 'Fail',
+                'Message': 'Can not delete'}, 400
